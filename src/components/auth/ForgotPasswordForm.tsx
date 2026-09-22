14e63588e1e280ff5255c59608b1163cc0461fd0
Stranger Mingle <strangermingleteam@gmail.com>
2026-09-23 00:29:55 +0530

    updated

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -60,7 +60,7 @@ export function ForgotPasswordForm() {
             {...register('email')}
             id="email"
             type="email"
-            className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent dark:border-zinc-700 dark:text-gray-50 dark:focus:ring-indigo-400"
+            className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent dark:border-zinc-700 dark:text-gray-50 dark:focus:ring-indigo-400"
             placeholder="name@example.com"
             disabled={isLoading || !!successMsg}
           />
